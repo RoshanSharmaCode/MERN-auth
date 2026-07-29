01f7711717ab8b2eaace308ff29d9c381dc906a1
Roshan <roshansharma9379@gmail.com>
2026-07-29 19:53:48 +0530

    created auth router and implemented nodemailer

--- server/server.js
+++ server/server.js
@@ -2,18 +2,21 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
-import connectDB from "./config/mongodb.js"
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
-connectDB()
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API Endpoints
 app.get("/", (req, res) => {
   res.send("API Working");
 });
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
