592d6fc8f9a5fa8ca36a3a004b81590fa84e7dbb
Roshan <roshansharma9379@gmail.com>
2026-08-04 16:14:22 +0530

    Created email templates

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,10 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
+import {
+  EMAIL_VERIFY_TEMPLATE,
+  PASSWORD_RESET_TEMPLATE,
+} from "../config/emailTemplates.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -136,7 +140,11 @@ export const sendVerifyOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Account Verification OTP",
-      text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+      // text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+      html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email,
+      ),
     };
     await transporter.sendMail(mailOptions);
 
@@ -227,7 +235,11 @@ export const sendResetOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Password Reset OTP",
-      text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password.`,
+      // text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password.`,
+      html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email,
+      ),
     };
     await transporter.sendMail(mailOptions);
 
@@ -244,12 +256,10 @@ export const resetPassword = async (req, res) => {
   const { email, otp, newPassword } = req.body;
 
   if (!email || !otp || !newPassword) {
-    return res
-      .status(404)
-      .json({
-        success: false,
-        message: "Email, OTP and new passowrd is required.",
-      });
+    return res.status(404).json({
+      success: false,
+      message: "Email, OTP and new passowrd is required.",
+    });
   }
 
   try {
@@ -277,12 +287,10 @@ export const resetPassword = async (req, res) => {
 
     user.save();
 
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message: "Password has been reset successfully.",
-      });
+    return res.status(200).json({
+      success: true,
+      message: "Password has been reset successfully.",
+    });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
